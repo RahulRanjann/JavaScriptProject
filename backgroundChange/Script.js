061f6b5ef7135fb67da2bcb68cbc3e75135affb6
Rahul Ranjan <88098666+rahul191311054@users.noreply.github.com>
2022-02-03 02:06:15 +0530

    facebookClone

--- backgroundChange/Script.js
+++ backgroundChange/Script.js
@@ -2,16 +2,18 @@ var remote = true;
 const layer_1 = document.getElementById("Layer_1");
 const day = document.getElementById("day");
 
-function kit() {
+layer_1.addEventListener("click", function kit() {
   if (remote) {
     layer_1.style.fill = "#ffff00";
     document.body.style.background = "black";
     remote = false;
-    day.innerHTML = "<p>Good night🌃</p>";
+    day.innerHTML = "<p class ="para">Good night🌃</p>";
+    document.querySelector(".para").style.color = "white"
   } else {
     layer_1.style.fill = "none";
     document.body.style.background = "none";
     remote = true;
-    day.innerHTML = "<p>Good morning🌄</p>";
+    day.innerHTML = "<p class = "para2">Good morning🌄</p>";
+    document.querySelector(".para").style.color = "black"
   }
-}
+});
