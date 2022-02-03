ebd21bbf12322e2eb54ca9f8eb0c0addf8f9d86c
Rahul Ranjan <88098666+rahul191311054@users.noreply.github.com>
2022-02-03 15:17:16 +0530

    fixbgchanger

--- backgroundChange/Script.js
+++ backgroundChange/Script.js
@@ -7,13 +7,13 @@ layer_1.addEventListener("click", function kit() {
     layer_1.style.fill = "#ffff00";
     document.body.style.background = "black";
     remote = false;
-    day.innerHTML = "<p class ="para">Good night🌃</p>";
-    document.querySelector(".para").style.color = "white"
+    // day.innerHTML = "<p class ="para">Good night🌃</p>";
+    document.querySelector(".para").style.color = "white";
   } else {
     layer_1.style.fill = "none";
     document.body.style.background = "none";
     remote = true;
-    day.innerHTML = "<p class = "para2">Good morning🌄</p>";
-    document.querySelector(".para").style.color = "black"
+    // day.innerHTML = "<p class = "para2">Good morning🌄</p>";
+    document.querySelector(".para").style.color = "black";
   }
 });
