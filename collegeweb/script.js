e669d2120443c8636e688dfa7dd56688ade5fa8a
Rahul Ranjan <88098666+rahul191311054@users.noreply.github.com>
2022-02-11 17:00:31 +0530

    cal , portfoliyo

---
+++ collegeweb/script.js
@@ -0,0 +1,10 @@
+const dic = [];
+var pera = document.getElementById("textChange").innerText;
+
+var typed = new Typed(".typing", {
+  strings: ["rahul", " a friend.", "a programmer.", "a Gammer.", "a chutiya."],
+  loop: true,
+  typeSpeed: 50,
+  backSpeed: 50,
+});
+
