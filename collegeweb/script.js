d386c4542b491be9c76fd7883bc243cbdec86177
Rahul Ranjan <88098666+rahul191311054@users.noreply.github.com>
2022-02-13 16:56:20 +0530

    all

--- collegeweb/script.js
+++ collegeweb/script.js
@@ -1,10 +1,16 @@
-const dic = [];
-var pera = document.getElementById("textChange").innerText;
-
 var typed = new Typed(".typing", {
-  strings: ["rahul", " a friend.", "a programmer.", "a Gammer.", "a chutiya."],
+  strings: ["Rahul", " a friend.", "a programmer.", "a Gamer."],
   loop: true,
   typeSpeed: 50,
   backSpeed: 50,
 });
+function clickFun(element) {
+  const pages = document.querySelectorAll(".pages");
+  pages.forEach(function (page) {
+    page.classList.remove("active");
+  });
 
+  const sect = document
+    .getElementById(element.dataset.active)
+    .classList.add("active");
+}
