5a9916769851196b49186c4e79b0d871eac41dbb
Rahul Ranjan <88098666+rahul191311054@users.noreply.github.com>
2022-02-13 21:32:37 +0530

    fix again

--- collegeweb/script.js
+++ collegeweb/script.js
@@ -1,16 +1,17 @@
+function clickFun(element) {
+  const pages = document.querySelectorAll(".pages");
+  pages.forEach(function (page) {
+    page.classList.remove("active");
+  });
+  console.log(element.dataset.active);
+  const sect = document
+    .getElementById(element.dataset.activePage)
+    .classList.add("active")
+}
+
 var typed = new Typed(".typing", {
   strings: ["Rahul", " a friend.", "a programmer.", "a Gamer."],
   loop: true,
   typeSpeed: 50,
   backSpeed: 50,
 });
-function clickFun(element) {
-  const pages = document.querySelectorAll(".pages");
-  pages.forEach(function (page) {
-    page.classList.remove("active");
-  });
-
-  const sect = document
-    .getElementById(element.dataset.active)
-    .classList.add("active");
-}
